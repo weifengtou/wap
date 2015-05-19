f92ef12ba133fef81c15e582c57930691c7eb56e
wodrow <wodrow451611cv@gmail.com>
2015-05-19 10:19:49 +0800

    tt

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,4 +1,15 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	/* URL配置 */
+    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
+    'URL_MODEL'            => 3, //URL模式
+    'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
+
+    /* 模板相关配置 */
+    'TMPL_PARSE_STRING' => array(
+        '__STATIC__' => __ROOT__ . '/Public/static',
+        '__SITE__' => __ROOT__ . '/Public/site',
+    ),
 );
