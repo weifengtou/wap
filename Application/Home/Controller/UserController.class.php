2ad06bf63b0ffa1bb716b5c40d712efd52544cc9
wodrow <wodrow451611cv@gmail.com>
2015-05-20 17:57:24 +0800

    tt

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -15,8 +15,21 @@ class UserController extends Controller {
     public function UserValidate()
     {
     	$valid = false;
-		echo json_encode(array(
+    	if (I("post.isSubmit")) {
+    		$username = I("post.username");
+    		$password = md5(I("post.password"));
+    		if ($userInfo = M("Homeuser")->where("username='$username' AND password='$password'")->select()) {
+    			$valid = true;
+    		}
+    	}
+		/*echo json_encode(array(
 		    'valid' => $valid,
-		));
+		));*/
+		if ($valid) {
+			session("userInfo",$userinfo);
+			echo "欢迎登陆微风投手机版!";
+		}else{
+			echo "用户名或密码错误!";
+		}
     }
 }
