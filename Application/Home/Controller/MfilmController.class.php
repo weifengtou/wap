1fb0a4964c6e8fa5559e69f38da9bb34b8e3160c
wodrow <wodrow451611cv@gmail.com>
2015-05-21 10:41:23 +0800

    tt

--- Application/Home/Controller/MfilmController.class.php
+++ Application/Home/Controller/MfilmController.class.php
@@ -1,8 +1,8 @@
 <?php
+namespace Home\Controller;
+class MfilmController extends HomeController {
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    public function index(){
+        $this->display();
+    }
+}
