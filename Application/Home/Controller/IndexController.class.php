1a0485b3428732356d4a302d36c6521f747373b4
wodrow <wodrow451611cv@gmail.com>
2015-05-26 10:49:57 +0800

    protpl

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,8 @@ namespace Home\Controller;
 class IndexController extends HomeController {
 
     public function index(){
+    	$projects = M('Project')->where("status=2")->select();
+    	$this->assign('projects',$projects);
         $this->display();
     }
 }
