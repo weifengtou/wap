5740ee84a2b622910f5eaec5d50933f0a03b52e9
wodrow <wodrow451611cv@gmail.com>
2015-06-01 16:18:00 +0800

    tt

--- Application/Home/Controller/ProjectController.class.php
+++ Application/Home/Controller/ProjectController.class.php
@@ -13,4 +13,19 @@ class ProjectController extends HomeController {
     	$this->assign('projects',$projects);
     	$this->display();
     }
+
+    public function proInfo($proId)
+    {
+    	if (!$proId) {
+    		$this->error("链接错误！");
+    	}else{
+    		$proInfo = M('Project')->where("id=".$proId)->limit("1")->select();
+    		if (!$proInfo) {
+    			$this->error("链接错误！");
+    		}else{
+    			$this->assign("proInfo",$proInfo);
+    			$this->display();
+    		}
+    	}
+    }
 }
